f75306eba22f91be0bc4506020d739607e6fe33c
Leticia Desiderio <letidesiderio18@gmail.com>
2022-04-05 10:41:51 -0300

    testa inserção de usuário

--- __test__/user.test.js
+++ __test__/user.test.js
@@ -9,3 +9,11 @@ test('Deve listar todos os usuários', () => {
         expect(res.body[0]).toHaveProperty('name', 'Luana Schotte');
     });
 });
+test('Deve inserir usuário com sucesso', () => {
+    return request(app).post('/users').send({name: "Lia Schotte", email: "lia@gmail.com"})
+    .then((res) => {
+        expect(res.status).toBe(201);
+        expect(res.body.name).toBe('Lia Schotte');
+        expect(res.body).toHaveProperty('email', 'lia@gmail.com');
+    });
+});
