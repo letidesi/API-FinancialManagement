9ea7be82dfecd2ea9675ad95727b7c2dbb95bcd2
Leticia Desiderio <letidesiderio18@gmail.com>
2022-04-09 09:07:38 -0300

    criar tabela de usuários

--- migrations/20220409114941_create_users.js
+++ migrations/20220409114941_create_users.js
@@ -2,7 +2,7 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = (knex) => {
+exports.up = function(knex) {
     return knex.schema.createTable('users', (t) => {
         t.increments('id').primary();
         t.string('name').notNull().unique();
@@ -15,6 +15,6 @@ exports.up = (knex) => {
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = (knex) => {
+exports.down = function(knex) {
     return knex.schema.dropTable('users');
 };
