e5c6b687ece363d2a686226d9da40cbfcf653516
Leticia Desiderio <letidesiderio18@gmail.com>
2022-04-12 11:04:20 -0300

    Criar método http de usuário

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,6 @@
 module.exports = (app) => {
     const findAll = (req, res) => {
-        app.db('users').select().then(result => res.status(200).json(result));
+        app.db('users').select().then((result) => res.status(200).json(result));
     };
 
     const create = async (req, res) => {
