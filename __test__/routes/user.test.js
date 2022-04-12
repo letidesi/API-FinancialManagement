e4d9cb9a616db79a88fc7a3456ce9ff03d4b1a32
Leticia Desiderio <letidesiderio18@gmail.com>
2022-04-12 11:00:57 -0300

    Criar teste de inserção e listagem de usuário

--- __test__/routes/user.test.js
+++ __test__/routes/user.test.js
@@ -5,15 +5,16 @@ const app = require('../../src/app');
 test('Deve listar todos os usuários', () => {
     return request(app).get('/users').then((res) => {
         expect(res.status).toBe(200);
-        expect(res.body).toHaveLength(1);
-        expect(res.body[0]).toHaveProperty('name', 'Luana Schotte');
+        expect(res.body.length).toBeGreaterThan(0);
     });
 });
 test('Deve inserir usuário com sucesso', () => {
-    return request(app).post('/users').send({ name: "Lia Schotte", email: "lia@gmail.com" })
-        .then(res => {
+    const email = `${Date.now()}@gmail.com`;
+    return request(app).post('/users').send({ name: 'Lia Schotte', email, password: '12345' })
+        .then((res) => {
             expect(res.status).toBe(201);
             expect(res.body.name).toBe('Lia Schotte');
-            expect(res.body).toHaveProperty('email', 'lia@gmail.com');
+            expect(res.body.password).toBe('12345');
+            expect(res.body).toHaveProperty('email');
         });
 });
