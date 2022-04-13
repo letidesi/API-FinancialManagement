8b03b918e445c60e419801510fcec9022944f623
Leticia Desiderio <letidesiderio18@gmail.com>
2022-04-13 13:54:25 -0300

    Retornar mensagem de email nulo

--- src/services/user.js
+++ src/services/user.js
@@ -3,7 +3,8 @@ module.exports = (app) => {
         return app.db('users').select();
     };
     const save = (user) => {
-        if(!user.name) return { error: 'Nome é um atributo obrigatório!'}
+        if(!user.name) return { error: 'Nome é um atributo obrigatório!'};
+        if(!user.email) return { error: 'E-mail é um atributo obrigatório!'};
         
         return app.db('users').insert(user, '*');
     };
