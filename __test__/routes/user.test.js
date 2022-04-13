4eb762059b74b76336cbce44a4be049e91c5e0b3
Leticia Desiderio <letidesiderio18@gmail.com>
2022-04-13 13:53:34 -0300

    Validar inserção de nome e email

--- __test__/routes/user.test.js
+++ __test__/routes/user.test.js
@@ -2,6 +2,17 @@ const request = require('supertest');
 
 const app = require('../../src/app');
 
+
+/* beforeAll(async () => {
+    //await db_Knex.connect();
+});
+afterAll(async () => {
+    //await setupDbDisconnect();
+});
+afterEach(async () => {
+    //await setupCollectionClean();
+}); */
+
 test('Deve listar todos os usuários', () => {
     return request(app).get('/users').then((res) => {
         expect(res.status).toBe(200);
@@ -17,4 +28,16 @@ test('Deve inserir usuário com sucesso', () => {
             expect(res.body.password).toBe('12345');
             expect(res.body).toHaveProperty('email');
         });
+});
+test('Não deve inserir usuário sem nome', () => {
+    return request(app).post('/users').send({ email: 'lisa@gmail.com', password: '12345' })
+        .then((res) => {
+            expect(res.status).toBe(400);
+            expect(res.body.error).toBe('Nome é um atributo obrigatório!')
+        });
+});
+test('Não deve inserir usuário sem email', async () => {
+    const result = await request(app).post('/users').send({  name: 'Lia Schotte', password: '12345' });
+    expect(result.status).toBe(400);
+    expect(result.body.error).toBe('E-mail é um atributo obrigatório!');
 });
