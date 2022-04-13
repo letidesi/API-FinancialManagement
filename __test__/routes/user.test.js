9aa4c2fd9e33db0fffdb67d12510ed753952ebf2
Leticia Desiderio <letidesiderio18@gmail.com>
2022-04-13 14:02:43 -0300

    Testar lista, inserção e validações do usuário

--- __test__/routes/user.test.js
+++ __test__/routes/user.test.js
@@ -13,31 +13,27 @@ afterEach(async () => {
     //await setupCollectionClean();
 }); */
 
-test('Deve listar todos os usuários', () => {
-    return request(app).get('/users').then((res) => {
-        expect(res.status).toBe(200);
-        expect(res.body.length).toBeGreaterThan(0);
-    });
+test('Deve listar todos os usuários', async () => {
+    const result = await request(app).get('/users');
+    expect(result.status).toBe(200);
+    expect(result.body.length).toBeGreaterThan(0);
 });
-test('Deve inserir usuário com sucesso', () => {
+test('Deve inserir usuário com sucesso', async () => {
     const email = `${Date.now()}@gmail.com`;
-    return request(app).post('/users').send({ name: 'Lia Schotte', email, password: '12345' })
-        .then((res) => {
-            expect(res.status).toBe(201);
-            expect(res.body.name).toBe('Lia Schotte');
-            expect(res.body.password).toBe('12345');
-            expect(res.body).toHaveProperty('email');
-        });
+    const result = await request(app).post('/users').send({ name: 'Lia Schotte', email, password: '12345' });
+    expect(result.status).toBe(201);
+    expect(result.body.name).toBe('Lia Schotte');
+    expect(result.body.password).toBe('12345');
+    expect(result.body).toHaveProperty('email');
 });
-test('Não deve inserir usuário sem nome', () => {
-    return request(app).post('/users').send({ email: 'lisa@gmail.com', password: '12345' })
-        .then((res) => {
-            expect(res.status).toBe(400);
-            expect(res.body.error).toBe('Nome é um atributo obrigatório!')
-        });
+test('Não deve inserir usuário sem nome', async () => {
+    const result = await request(app).post('/users').send({ email: 'lisa@gmail.com', password: '12345' });
+    expect(result.status).toBe(400);
+    expect(result.body.error).toBe('Nome é um atributo obrigatório!')
+
 });
 test('Não deve inserir usuário sem email', async () => {
-    const result = await request(app).post('/users').send({  name: 'Lia Schotte', password: '12345' });
+    const result = await request(app).post('/users').send({ name: 'Lia Schotte', password: '12345' });
     expect(result.status).toBe(400);
     expect(result.body.error).toBe('E-mail é um atributo obrigatório!');
 });
