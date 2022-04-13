6137502e2ec356bcf137c866c47abf652990dd1e
Leticia Desiderio <letidesiderio18@gmail.com>
2022-04-13 16:51:08 -0300

    Acrescentar descrição do teste

--- __test__/routes/user.test.js
+++ __test__/routes/user.test.js
@@ -1,43 +1,49 @@
 const request = require('supertest');
-
 const app = require('../../src/app');
 
+const email = `${Date.now()}@gmail.com`;
 
-/* beforeAll(async () => {
-    //await db_Knex.connect();
-});
-afterAll(async () => {
-    //await setupDbDisconnect();
-});
-afterEach(async () => {
-    //await setupCollectionClean();
-}); */
+describe('Teste de usuário', () => {
+    /*  beforeAll(async () => {
+         await connect();
+     });
+     afterAll(async () => {
+         await setupDbDisconnect();
+     });
+     afterEach(async () => {
+         await setupCollectionClean();
+     }); */
 
-test('Deve listar todos os usuários', async () => {
-    const result = await request(app).get('/users');
-    expect(result.status).toBe(200);
-    expect(result.body.length).toBeGreaterThan(0);
-});
-test('Deve inserir usuário com sucesso', async () => {
-    const email = `${Date.now()}@gmail.com`;
-    const result = await request(app).post('/users').send({ name: 'Lia Schotte', email, password: '12345' });
-    expect(result.status).toBe(201);
-    expect(result.body.name).toBe('Lia Schotte');
-    expect(result.body.password).toBe('12345');
-    expect(result.body).toHaveProperty('email');
-});
-test('Não deve inserir usuário sem nome', async () => {
-    const result = await request(app).post('/users').send({ email: 'lisa@gmail.com', password: '12345' });
-    expect(result.status).toBe(400);
-    expect(result.body.error).toBe('Nome é um atributo obrigatório!');
-});
-test('Não deve inserir usuário sem email', async () => {
-    const result = await request(app).post('/users').send({ name: 'Lia Schotte', password: '12345' });
-    expect(result.status).toBe(400);
-    expect(result.body.error).toBe('E-mail é um atributo obrigatório!');
-});
-test('Não deve inserir usuário sem senha', async () => {
-    const result = await request(app).post('/users').send({ name: 'Lia Schotte',  email: 'lisa@gmail.com', });
-    expect(result.status).toBe(400);
-    expect(result.body.error).toBe('Senha é um atributo obrigatório!');
+    test('Deve listar todos os usuários', async () => {
+        const result = await request(app).get('/users');
+        expect(result.status).toBe(200);
+        expect(result.body.length).toBeGreaterThan(0);
+    });
+    test('Deve inserir usuário com sucesso', async () => {
+        const result = await request(app).post('/users').send({ name: 'Lia Schotte', email, password: '12345' });
+        expect(result.status).toBe(201);
+        expect(result.body.name).toBe('Lia Schotte');
+        expect(result.body.password).toBe('12345');
+        expect(result.body).toHaveProperty('email');
+    });
+    test('Não deve inserir usuário sem nome', async () => {
+        const result = await request(app).post('/users').send({ email: 'lisa@gmail.com', password: '12345' });
+        expect(result.status).toBe(400);
+        expect(result.body.error).toBe('Nome é um atributo obrigatório!');
+    });
+    test('Não deve inserir usuário sem email', async () => {
+        const result = await request(app).post('/users').send({ name: 'Lia Schotte', password: '12345' });
+        expect(result.status).toBe(400);
+        expect(result.body.error).toBe('E-mail é um atributo obrigatório!');
+    });
+    test('Não deve inserir usuário sem senha', async () => {
+        const result = await request(app).post('/users').send({ name: 'Lia Schotte', email: 'lisa@gmail.com', });
+        expect(result.status).toBe(400);
+        expect(result.body.error).toBe('Senha é um atributo obrigatório!');
+    });
+    test('Não deve inserir usuário com email existente', async () => {
+        const result = await request(app).post('/users').send({ name: 'Lia Schotte', email, password: '12345' });
+        expect(result.status).toBe(400);
+        expect(result.body.error).toBe('Já existe um usuário com este e-mail!');
+    });
 });
