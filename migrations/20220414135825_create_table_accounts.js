77321f436ac9d7a3d062b8b3ba4a2fc1ffbb22a2
Leticia Desiderio <letidesiderio18@gmail.com>
2022-04-14 13:01:07 -0300

    fix(tableAccount): Alterando o nome da string

--- migrations/20220414135825_create_table_accounts.js
+++ migrations/20220414135825_create_table_accounts.js
@@ -5,7 +5,7 @@
 exports.up = (knex) => {
     return knex.schema.createTable('accounts', (t) => {
         t.increments('id').primary();
-        t.string('nome').notNull();
+        t.string('name').notNull();
         t.integer('user_id').references('id').inTable('users').notNull();
     });
 };
