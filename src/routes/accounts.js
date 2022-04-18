89d105d002795de2305ffa85d798a055b4f97293
Leticia Desiderio <letidesiderio18@gmail.com>
2022-04-18 14:54:45 -0300

    feat: Criando o método de busca da conta

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -4,7 +4,11 @@ module.exports = (app) => {
             return res.status(201).json(result[0]);
         });
     };
+
+    const findAll = (req, res) => {
+        app.services.account.findAll().then(result => res.status(200).json(result));
+    }
     
-    return { create };
+    return { create, findAll };
 };
 
