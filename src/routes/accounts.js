2ae3ca92e66b0fe5c5ead7d1f7ef5c428146be9b
Leticia Desiderio <letidesiderio18@gmail.com>
2022-04-19 10:50:12 -0300

    feat: Criando método de buscar conta por id

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -8,7 +8,12 @@ module.exports = (app) => {
     const findAll = (req, res) => {
         app.services.account.findAll().then(result => res.status(200).json(result));
     }
-    
-    return { create, findAll };
+
+    const get = (req, res) => {
+        app.services.account.find({id: req.params.id})
+            .then(result => res.status(200).json(result));
+    };
+
+    return { create, findAll, get };
 };
 
